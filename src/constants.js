d92041d6d1cdb31821c0f5897d619cfd0185647d
Matthew-SA <surfsnow1@gmail.com>
2020-01-29 18:49:17 -0800

    fix BORDERRIGHT constant

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ export const FPS = 60;
 
 export const BORDERTOP = 168;
 export const BORDERLEFT = 0;
-export const BORDERRIGHT = 696;
+export const BORDERRIGHT = 720;
 export const BORDERBOTTOM = 648;
