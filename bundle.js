e2e04e79a753ff27795051598c366300ac03130d
Matthew-SA <surfsnow1@gmail.com>
2020-02-08 18:36:15 -0800

    complete current octorok refactor

--- bundle.js
+++ bundle.js
@@ -999,9 +999,10 @@ class Octorok {
   constructor(pixelPos, grid) {
     this.sprite = new Image();
     this.sprite.src = "./assets/images/units/octorok.png"
-
+    
     this.grid = grid;
-
+    
+    //position data
     this.lastPixelPos = pixelPos;
     this.pixelPos = pixelPos;
     this.pos = [
@@ -1009,59 +1010,37 @@ class Octorok {
       ((this.pixelPos[1] - 168) / 48)
     ]
     
+    // frame data
     this.runCycle = 0;
     this.actionCycle = 48;
     this.direction = 0
     this.frame = 0;
-    this.queueMovement();
+    this.speed = _util__WEBPACK_IMPORTED_MODULE_0__["random"](1,3)
+    
+    //start action cycle
+    this.updateAction();
+  }
+  
+  clear(ctx) {
+    ctx.clearRect(this.lastPixelPos[0], this.lastPixelPos[1], 48, 48);
   }
 
   step() {
-    if (this.actionCycle <= 0) this.queueMovement();
+    this.lastPixelPos = this.pixelPos;
+
+    if (this.actionCycle <= 0) this.updateAction();
 
     if (this.direction === 102) { // north
-      this.pixelPos[1] -= 1
+      this.pixelPos[1] -= 1 * this.speed
     } else if (this.direction === 153) { // east
-      this.pixelPos[0] += 1
+      this.pixelPos[0] += 1 * this.speed
     } else if (this.direction === 0) { // south
-      this.pixelPos[1] += 1
+      this.pixelPos[1] += 1 * this.speed
     } else if (this.direction === 51) { // west
-      this.pixelPos[0] -= 1
+      this.pixelPos[0] -= 1 * this.speed
     }
-    this.actionCycle -= 1
+    this.actionCycle -= 1 * this.speed
   }
-
-  checkAvailableActions() {
-    let neighbors = [];
-    if (this.pos[1] > 0 && this.grid[this.pos[1] - 1][this.pos[0]] === 1020) {
-      neighbors.push([102, 0, -1]); // north
-    }
-    if (this.grid[this.pos[1]][this.pos[0] + 1] === 1020) {
-      neighbors.push([153, 1, 0]); // east
-    }
-    if (this.pos[1] < 10 && this.grid[this.pos[1] + 1][this.pos[0]] === 1020) {
-      neighbors.push([0, 0, 1]); // south
-    }
-    if (this.grid[this.pos[1]][this.pos[0] - 1] === 1020) {
-      neighbors.push([51, -1, 0]); // west
-    }
-    return neighbors;
-  }
-
-  queueMovement() {
-    let possibleActions = this.checkAvailableActions();
-    this.actionCycle = 48;
-    let action = _util__WEBPACK_IMPORTED_MODULE_0__["sample"](possibleActions);
-    console.log(action)
-    this.direction = action[0];
-    this.pos[0] += action[1];
-    this.pos[1] += action[2];
-  }
-
-  // update(x, y) {
-  //   this.pos[0] += x;
-  //   this.pos[1] += y;
-  // }
 
   draw(ctx) {
     if (this.runCycle < 14) {
@@ -1082,111 +1061,36 @@ class Octorok {
       48,
       48
       )
-      this.lastPixelPos[0] = this.pixelPos[0];
-      this.lastPixelPos[1] = this.pixelPos[1];
     }
 
-  clear(ctx) {
-    ctx.clearRect(this.lastPixelPos[0], this.lastPixelPos[1], 48, 48);
+  checkAvailableActions() {
+    let neighbors = [];
+    if (this.pos[1] > 0 && this.grid[this.pos[1] - 1][this.pos[0]] === 1020) {
+      neighbors.push([102, 0, -1]); // north
+    }
+    if (this.grid[this.pos[1]][this.pos[0] + 1] === 1020) {
+      neighbors.push([153, 1, 0]); // east
+    }
+    if (this.pos[1] < 10 && this.grid[this.pos[1] + 1][this.pos[0]] === 1020) {
+      neighbors.push([0, 0, 1]); // south
+    }
+    if (this.grid[this.pos[1]][this.pos[0] - 1] === 1020) {
+      neighbors.push([51, -1, 0]); // west
+    }
+    return neighbors;
+  }
+
+  updateAction() {
+    let possibleActions = this.checkAvailableActions();
+    this.actionCycle = 48;
+    let action = _util__WEBPACK_IMPORTED_MODULE_0__["sample"](possibleActions);
+    this.direction = action[0];
+    this.pos[0] += action[1];
+    this.pos[1] += action[2];
   }
 }
 
 /* harmony default export */ __webpack_exports__["default"] = (Octorok);
-
-
-// import * as util from '../util'
-
-// class Octorok {
-//   constructor(pos, grid) {
-//     this.lastPos = pos
-//     this.pos = pos;
-//     this.grid = grid;
-//     this.gridRow = ((pos[1] - 168) / 48)
-//     this.gridCol = pos[0] / 48
-//     this.sprite = new Image();
-//     this.sprite.src = "./assets/images/units/octorok.png"
-//     this.runCycle = 0;
-//     this.actionCycle = 48;
-//     this.direction = 0
-//     this.frame = 0;
-//     this.queueMovement();
-//     console.log(this.gridRow)
-//     console.log(this.gridCol)
-//   }
-
-//   step() {
-//     if (this.actionCycle <= 0) {
-//       this.gridRow = Math.floor((this.pos[1] - 168) / 48);
-//       this.gridCol = Math.floor(this.pos[0] / 48);
-//       this.queueMovement();
-//     }
-//     if (this.direction === 102) { // north
-//       this.pos[1] -= 1
-//     } else if (this.direction === 153) { // east
-//       this.pos[0] += 1
-//     } else if (this.direction === 0) { // south
-//       this.pos[1] += 1
-//     } else if (this.direction === 51) { // west
-//       this.pos[0] -= 1
-//     }
-//     this.actionCycle -= 1
-//   }
-
-//   checkAvailableActions() {
-//     let neighbors = [];
-//     this.gridRow = Math.floor(this.gridRow)
-//     this.gridCol = Math.floor(this.gridCol)
-//     if (this.gridRow > 0) {
-//       if (this.grid[this.gridRow - 1][this.gridCol] === 0) neighbors.push(102); // north
-//     }
-//     if (this.grid[this.gridRow][this.gridCol + 1] === 0) neighbors.push(153); // east
-//     if (this.gridRow < 10) {
-//       if (this.grid[this.gridRow + 1][this.gridCol] === 0) neighbors.push(0); // south
-//     }
-//     if (this.grid[this.gridRow][this.gridCol - 1] === 0) neighbors.push(51); // west
-//     return neighbors;
-//   }
-
-//   queueMovement() {
-//     let actions = this.checkAvailableActions();
-//     this.actionCycle = 48;
-//     this.direction = util.sample(actions);
-//   }
-
-//   update(x, y) {
-//     this.pos[0] += x;
-//     this.pos[1] += y;
-//   }
-
-//   draw(ctx) {
-//     if (this.runCycle < 14) {
-//       this.frame = 0;
-//     } else {
-//       this.frame = 51;
-//     }
-//     if (this.runCycle > 25) this.runCycle = 0;
-//     if (this.attacking) this.frame = 153;
-//     ctx.drawImage(
-//       this.sprite,
-//       this.direction,
-//       this.frame,
-//       48,
-//       48,
-//       this.pos[0],
-//       this.pos[1],
-//       48,
-//       48
-//       )
-//       this.lastPos[0] = this.pos[0];
-//       this.lastPos[1] = this.pos[1];
-//     }
-
-//   clear(ctx) {
-//     ctx.clearRect(this.lastPos[0], this.lastPos[1], 48, 48);
-//   }
-// }
-
-// export default Octorok;
 
 /***/ }),
 
