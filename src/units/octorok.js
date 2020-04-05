ff8050d48254129da56cdb1e47213514ad4111ea
Matthew-SA <surfsnow1@gmail.com>
2020-04-05 10:42:08 -0700

    add more enemy skins

--- src/units/octorok.js
+++ src/units/octorok.js
@@ -2,11 +2,11 @@ import * as Util from '../util/util'
 import Unit from './unit'
 class Octorok extends Unit {
   constructor(pixelPos, grid, ctx) {
-    let type = Util.random(0, 2)
+    let type = Util.random(0, 4)
     super(pixelPos, grid, (type * 96), ctx)
     
     // unit stats
-    this.hp = type === 0 ? 1 : 2;
+    this.hp = (type === 0 || type === 2) ? 1 : 2;
     // this.ap = 1;
 
     this.speed = Util.random(1,3)
